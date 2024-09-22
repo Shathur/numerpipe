c5428e1048843d9268d1654cfcadfb3180becf58
Shathur <iliaslangas@gmail.com>
2024-09-22 23:23:05 +0300

    check if model exists and saving with Path instead of string

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import os
 from lightgbm import LGBMRegressor
 from xgboost import XGBRegressor, XGBClassifier
 import json
+from pathlib import Path
 
 
 # get models into a list for iteration on them
@@ -20,9 +21,11 @@ def get_model_lst(num_models=1, prefix=None, folder_name=None, verbose=True):
 
     """
     # model_lst = [folder_name + x for x in os.listdir(folder_name)]
-    model_lst = [os.path.join(folder_name, x) for x in os.listdir(os.path.join(folder_name))]
+    model_lst = [
+        os.path.join(folder_name, x) for x in os.listdir(os.path.join(folder_name))
+    ]
     if prefix is not None:
-        model_lst = [x for x in model_lst if x.startswith(folder_name+prefix)]
+        model_lst = [x for x in model_lst if x.startswith(folder_name + prefix)]
     else:
         pass
     if num_models != 0:
@@ -38,15 +41,21 @@ def get_model_lst(num_models=1, prefix=None, folder_name=None, verbose=True):
 def run_model(
     train_data=None,
     val_data=None,
-    model_type='xgb',
-    task_type='regression',
+    model_type="xgb",
+    task_type="regression",
     model_params=None,
     fit_params=None,
-    save_to_drive=False, 
+    save_to_drive=False,
     save_folder=None,
     legacy_save=True,
     cv_count=None,
 ):
+    # check if our save folder exists
+    if save_to_drive:
+        assert (
+            save_folder is not None
+        ), "save_folder must be provided when save_to_drive is True"
+        assert os.path.exists(save_folder), "save_folder does not exist"
     X_train, y_train = train_data
     X_val, y_val = val_data
 
@@ -57,85 +66,100 @@ def run_model(
     )
 
     if fit_params is None:
-        fit_params = {
-            'early_stopping_rounds': 10,
-            'verbose': False
-        }
+        fit_params = {"early_stopping_rounds": 10, "verbose": False}
     else:
         pass
 
     model.fit(
-        X_train, y_train,
+        X_train,
+        y_train,
         eval_set=[(X_val, y_val)],
         **fit_params,
     )
 
     # if we are using lgb we first need to keep the booster of our model
-    if model_type == 'lgb':
+    if model_type == "lgb":
         model = model.booster_
 
     if save_to_drive:
         if legacy_save:
-            if model_type=='lgb':
-                model.save_model(os.path.join(save_folder,f'model_{cv_count}.{model_type}'))
+            if model_type == "lgb":
+                model.save_model(
+                    os.path.join(
+                        Path(Path(save_folder) / f"model_{cv_count}.{model_type}")
+                    )
+                )
             else:
-                model.save_model(save_folder + 'model_{}.'.format(cv_count)+model_type)
+                model.save_model(
+                    Path(Path(save_folder) / "model_{}.".format(cv_count) / model_type)
+                )
         else:
-            if model_type=='lgb':
+            if model_type == "lgb":
                 model_json = model.dump_model()
-                with open(os.path.join(save_folder,f'model_{cv_count}.{model_type}'),'w') as f:
+                with open(
+                    os.path.join(
+                        Path(Path(save_folder) / f"model_{cv_count}.{model_type}")
+                    ),
+                    "w",
+                ) as f:
                     json.dump(model_json, f)
-                model.save_model(os.path.join(save_folder,f'model_{cv_count}.{model_type}'))
+                model.save_model(
+                    os.path.join(
+                        Path(Path(save_folder) / f"model_{cv_count}.{model_type}")
+                    )
+                )
             else:
-                model.save_model(save_folder + 'model_{}.json'.format(cv_count))
-     # else:
-     #     if legacy_save:
-     #         model.save_model('model_{}.'.format(cv_count)+model_type)
-     #     else:
-     #         model.save_model('model_{}.json'.format(cv_count))
+                model.save_model(
+                    Path(Path(save_folder) / "model_{}.json".format(cv_count))
+                )
+    # else:
+    #     if legacy_save:
+    #         model.save_model('model_{}.'.format(cv_count)+model_type)
+    #     else:
+    #         model.save_model('model_{}.json'.format(cv_count))
 
     return model
 
 
-def create_model(model_type='xgb', task_type='regression', model_params=None):
+def create_model(model_type="xgb", task_type="regression", model_params=None):
     if model_params is None:
         model_params = get_default_params(model_type=model_type)
     else:
         pass
-    if model_type == 'lgb':
+    if model_type == "lgb":
         model = LGBMRegressor()
         model.set_params(**model_params)
 
-    if model_type == 'xgb':
+    if model_type == "xgb":
         model = XGBRegressor()
         model.set_params(**model_params)
 
-    if task_type == 'classification':
+    if task_type == "classification":
         model = XGBClassifier()
         model.set_params(**model_params)
 
     return model
 
 
-def get_default_params(model_type='xgb'):
-    if model_type == 'lgb':
+def get_default_params(model_type="xgb"):
+    if model_type == "lgb":
         params = {
-            'learning_rate': 0.01,
-            'n_estimators': 1000,
-            'num_leaves': 2 ** 5,
-            'max_depth': 5,
-            'colsample_bytree': 0.6,
-            'device': "gpu",
+            "learning_rate": 0.01,
+            "n_estimators": 1000,
+            "num_leaves": 2**5,
+            "max_depth": 5,
+            "colsample_bytree": 0.6,
+            "device": "gpu",
         }
-    elif model_type == 'xgb':
+    elif model_type == "xgb":
         params = {
-            'max_depth': 5,
-            'learning_rate': 0.01,
-            'n_estimators': 1000,
-            'n_jobs': -1,
-            'colsample_bytree': 0.6,
-            'tree_method': 'gpu_hist',
-            'verbosity': 0
+            "max_depth": 5,
+            "learning_rate": 0.01,
+            "n_estimators": 1000,
+            "n_jobs": -1,
+            "colsample_bytree": 0.6,
+            "tree_method": "gpu_hist",
+            "verbosity": 0,
         }
 
     return params
